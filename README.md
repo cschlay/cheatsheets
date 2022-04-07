cc9e2af8e0ac0c4f5fca0eb1f8ce884d1b6e3f1d
Chi-Hao Lay <chi-hao.lay@outlook.com>
2022-04-07 17:33:34 +0300

    Fix styles

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 Nobody remembers all commands.
 A curated list reduces the googling of same snippets.
+
+```bash
+npm run dev
+```
+
+## Structure
+
+Everything is coded in raw HTML or declared as a _Web Component_.
--- public/js/components.js
+++ public/js/components.js
@@ -1,28 +1,29 @@
 class Menu extends HTMLElement {
-    constructor() {
-        super();
-        this.className = "menu";
-        this.innerHTML = `
-            <a href="index.html">Home</a>
-            <a href="aspnet-core.html">ASP.NET Core</a>
-            <a href="react.html">React</a>
-            <a href="jest.html">Jest</a>
-            <a href="playwright.html">Playwright</a>
-            <a href="cypress.html">Cypress</a>
-        `;
-    }
+  constructor() {
+    super();
+    this.className = "menu";
+    this.innerHTML = `
+      <a href="index.html">General</a>
+      <a href="aspnet-core.html">ASP.NET Core</a>
+      <a href="react.html">React</a>
+      <a href="jest.html">Jest</a>
+      <a href="playwright.html">Playwright</a>
+      <a href="cypress.html">Cypress</a>
+      <a href="devops.html">DevOps</a>
+    `;
+  }
 }
 
 class Footer extends HTMLElement {
-    constructor() {
-        super();
-        this.innerHTML = `
-            <footer>
-                Licensed under
-                <a href="https://github.com/cschlay/cheatsheets/blob/main/LICENSE">The Unlicense</a>,
-            </footer>
-        `;
-    }
+  constructor() {
+    super();
+    this.innerHTML = `
+      <footer>
+          Licensed under
+          <a href="https://github.com/cschlay/cheatsheets/blob/main/LICENSE">The Unlicense</a>
+      </footer>
+    `;
+  }
 }
 
 window.customElements.define("x-footer", Footer);
